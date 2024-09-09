7047e2c138b882c69beb83f679dc1bc2de0b8aba
Hanis <hanishassim@users.noreply.github.com>
2024-09-09 11:24:02 +0800

    Project init

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Hassim, Muhammad Hanis on 09/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private(set) var codePushButton: UIButton!
+    @IBOutlet private(set) var codeModalButton: UIButton!
+    @IBOutlet private(set) var seguePushButton: UIButton!
+    @IBOutlet private(set) var segueModalButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction private func pushNextViewController() {
+        let nextVC = CodeNextViewController(labelText: "Pushed from code")
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @IBAction private func presentModalNextViewController() {
+        let nextVC = CodeNextViewController(labelText: "Modal from code")
+        self.present(nextVC, animated: true)
+    }
+
+}
+
