0835cae3b05ba5bfe06c7f58711aab4e607c5f2b
Hanis <hanishassim@users.noreply.github.com>
2024-09-09 14:30:07 +0800

    Test code-based push navigation

--- NavigationTests/TestHelpers.swift
+++ NavigationTests/TestHelpers.swift
@@ -10,3 +10,7 @@ import UIKit
 func tap(_ button: UIButton) {
     button.sendActions(for: .touchUpInside)
 }
+
+func executeRunLoop() {
+    RunLoop.current.run(until: Date())
+}
--- NavigationTests/ViewControllerTests.swift
+++ NavigationTests/ViewControllerTests.swift
@@ -2,6 +2,7 @@
 import XCTest
 
 final class ViewControllerTests: XCTestCase {
+    /// A good test name must have acton and then the outcome or effect
     func test_tappingCodePushButton_shouldPushCodeNextViewController() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let sut: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
@@ -10,6 +11,16 @@ final class ViewControllerTests: XCTestCase {
         
         tap(sut.codePushButton)
         
+        executeRunLoop() // Needed for handling event like push navigation to allow it to take effect
+        
         XCTAssertNotNil(sut.navigationController) // To check if sut is contained in navigationController
+        XCTAssertEqual(navigation.viewControllers.count, 2, "navigation stack")
+        
+        let pushedVC = navigation.viewControllers.last
+        guard let codeNextVC = pushedVC as? CodeNextViewController else {
+            XCTFail("Expected CodeNextViewController, " + "but was \(String(describing: pushedVC))")
+            return
+        }
+        XCTAssertEqual(codeNextVC.label.text, "Pushed from code")
     }
 }
